7bff237fd93b93a0841e7420db9242ac8d9a0553
thiya-1st <taingarasothy@gmail.com>
2026-07-16 00:22:06 +0100

    backtest best pair

---
+++ src/backtest.py
@@ -0,0 +1,6 @@
+def calculate_position_size(capital, beta, price_y, price_x):
+    dollars_in_y = capital / (1 + beta)
+    dollars_in_x = capital - dollars_in_y
+    shares_y = dollars_in_y / price_y
+    shares_x = dollars_in_x / price_x
+    return shares_x, shares_y
